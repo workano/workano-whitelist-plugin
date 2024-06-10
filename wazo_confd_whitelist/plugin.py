acb486db498d1a9fc8fa9d1cc49d68f0f765288b
m.rezvani <mohsen.rezvani.rad33@gmail.com>
2024-06-10 11:40:57 +0330

    chagne plugin.py

--- wazo_confd_whitelist/plugin.py
+++ wazo_confd_whitelist/plugin.py
@@ -5,25 +5,29 @@ from .services import build_whitelist_service
 from .db import init_db
 
 logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
 class Plugin:
     def load(self, dependencies):
         logger.info('Whitelist plugin loading')
-        init_db('postgresql://asterisk:proformatique@localhost/asterisk?application_name=wazo-whitelist-plugin')
-        api = dependencies['api']
-        whitelist_service = build_whitelist_service()
+        try:
+            init_db('postgresql://asterisk:proformatique@localhost/asterisk?application_name=wazo-whitelist-plugin')
+            api = dependencies['api']
+            whitelist_service = build_whitelist_service()
 
-        # Whitelists
-        api.add_resource(
-            WhitelistList,
-            '/whitelists',
-            resource_class_args=(whitelist_service,)
-        )
-        api.add_resource(
-            WhitelistItem,
-            '/whitelists/<int:id>',
-            endpoint='whitelists',
-            resource_class_args=(whitelist_service,)
-        )
+            # Register API resources
+            api.add_resource(
+                WhitelistList,
+                '/whitelists',
+                resource_class_args=(whitelist_service,)
+            )
+            api.add_resource(
+                WhitelistItem,
+                '/whitelists/<int:id>',
+                endpoint='whitelists',
+                resource_class_args=(whitelist_service,)
+            )
 
-        logger.info('Whitelist plugin loaded')
+            logger.info('Whitelist plugin loaded successfully')
+        except Exception as e:
+            logger.error(f'Error loading whitelist plugin: {e}', exc_info=True)
